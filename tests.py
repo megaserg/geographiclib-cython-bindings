3c04b3cfe8bdadd85088d9c5f487f2577cb5b04a
Sergey Serebryakov <sergey@serebryakov.info>
2017-09-07 14:25:03 -0700

    Add tests

---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+import pytest
+from geographiclib.geodesic import Geodesic
+from geographiclib_cython import Geodesic as CythonGeodesic
+
+# Run with: python -m pytest tests.py
+
+class TestGeodesic(unittest.TestCase):
+    def test_inverse(self):
+        actual = CythonGeodesic.WGS84.Inverse(10, 20, 30, 40)
+        expected = Geodesic.WGS84.Inverse(10, 20, 30, 40)
+
+        assert actual['s12'] == pytest.approx(expected['s12'], 1e-10)
+        assert actual['azi1'] == pytest.approx(expected['azi1'], 1e-10)
+        assert actual['azi2'] == pytest.approx(expected['azi2'], 1e-10)
+
+    def test_direct(self):
+        actual = CythonGeodesic.WGS84.Direct(10, 20, 30, 4000)
+        expected = Geodesic.WGS84.Direct(10, 20, 30, 4000)
+
+        assert actual['lat2'] == pytest.approx(expected['lat2'], 1e-10)
+        assert actual['lon2'] == pytest.approx(expected['lon2'], 1e-10)
+        assert actual['azi2'] == pytest.approx(expected['azi2'], 1e-10)
+
+    def test_inverse_line(self):
+        actual_line = CythonGeodesic.WGS84.InverseLine(10, 20, 30, 40)
+        expected_line = Geodesic.WGS84.InverseLine(10, 20, 30, 40)
+
+        assert actual_line.s13 == pytest.approx(expected_line.s13, 1e-10)
+
+        actual_pos = actual_line.Position(100000)
+        expected_pos = expected_line.Position(100000)
+        assert actual_pos['lat2'] == pytest.approx(expected_pos['lat2'], 1e-10)
+        assert actual_pos['lon2'] == pytest.approx(expected_pos['lon2'], 1e-10)
